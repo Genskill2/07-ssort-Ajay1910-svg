54a7ed8806ce63d6adcb992c7eadf92f30d355d1
Ajay1910-svg <84716743+Ajay1910-svg@users.noreply.github.com>
2021-06-26 12:26:43 +0530

    Update ssort.c

--- ssort.c
+++ ssort.c
@@ -1,4 +1,4 @@
-void swap_max(int arr[],int l,int n)
+int swap_max(int arr[],int l,int n)
 {
   int max=0;
   for(n=1;n<=l;n++)
@@ -9,4 +9,16 @@ void swap_max(int arr[],int l,int n)
     }
   }
   return(max);
+}
+void ssort(int arr[],int l)
+{
+  int temp,big,n;
+  for(n=l-1;n>=1;n--)
+  {
+    big=swap_max(arr,l,n);
+    temp=arr[big];
+    arr[big]=arr[n];
+    arr[n]=temp;
+  }
+  return;
 }
