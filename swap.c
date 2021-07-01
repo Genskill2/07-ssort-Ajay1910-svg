affbc8a15e4369cf050369abdbae662545383711
Ajay1910-svg <84716743+Ajay1910-svg@users.noreply.github.com>
2021-07-01 09:58:45 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,29 +1,22 @@
 #include<stdio.h>
-void swap_max(int arr[],int l,int n)
-{
-  int max=n;
-  int temp=0;
-  for(int i=n;i<l;i++)
-  {
-    if(arr[i+1]>arr[max])
-    {
-      max=i+1;
-    }
-  }
-  temp=arr[max];
-  arr[max]=arr[n];
-  arr[n]=temp;
+#include<cs50.h>
+int swap_max(int arr[], int l, int n){
+	int max=arr[n];
+	int pos=n;
+	for(int i=n;i<l;i++){
+	  if(arr[i]>max){
+		max=arr[i];
+		pos=i;}}
+	arr[pos]=arr[n];
+	arr[n]=max;
+	
 }
-void ssort(int arr[],int l)
-{
-  for(int i=0;i<l;i++)
-  {
-   swap_max(arr,l,i);
-  }
-  for(int i=0;i<l;i++)
-  {
-    printf("|%i|",arr[i]);
-  }
-  printf("\n");
+int ssort(int arr[], int l){
+	for(int i=0; i<l;i++){
+	swap_max(arr, l, i);}
+	printf("the sorted array is \n|");
+	for(int i=0; i<9;i++)
+	printf(" %i |",arr[i]);
+	printf("\n");
+	
 }
-
